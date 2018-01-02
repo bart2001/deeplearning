d0b5941e7651e223ee112efc5e7f89e3ed86c5f9
bart2001 <bart2001@naver.com>
2018-01-02 10:20:48 +0900

    프로젝트병 변경

--- lab6/softmax_cross_entropy_with_logits.py
+++ lab6/softmax_cross_entropy_with_logits.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
 import numpy as np
 
-xy = np.loadtxt('data-04-zoo.csv', delimiter=',', dtype=np.float32)
+xy = np.loadtxt('data-04-zoo.csv', delimiter=',', dtype=np.float32)\
+
+x_data = xy[:, 0:-1]
+y_data = xy[:, [-1]]
+
+# logits = tf.matmul(X, W) + b
